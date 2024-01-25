e58b808c4bd679d1607a4cc42308cfadf03ba8a0
Kevin Wu <kevinwu098@gmail.com>
2024-01-24 21:12:37 -0800

    feat: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,15 @@ import Link from "next/link";
 const Header = () => {
     return (
         <>
-            <nav className="wrapper flex-between inset-x-0 z-30 h-24">
+            <nav className="wrapper sticky flex-between inset-x-0 z-30 h-24">
                 <div className="flex items-center text-3xl font-bold text-primary gap-x-4">
-                    <Image
-                        src="https://studentcouncil.ics.uci.edu/assets/img/logo.svg"
-                        alt="logo"
-                        width={64}
-                        height={64}
-                    />
-                    <Link href="/">
-                        <h1>Brain Games</h1>
+                    <Link href={"/"}>
+                        <Image
+                            src="/Logo_Without_BG.png"
+                            alt="logo"
+                            width={64}
+                            height={64}
+                        />
                     </Link>
                 </div>
             </nav>
