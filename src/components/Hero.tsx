6bbc6b61ee1400838bb8ca50fce505950edd360c
sirAvent <chiu7a@gmail.com>
2024-01-24 21:44:02 -0800

    responsiveness + graphics

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,25 +7,25 @@ const Hero = () => {
         <div className="flex-center flex-col gap-y-16 min-h-[calc(100vh-6rem)]">
             <div className="flex flex-col gap-y-8">
                 <div className="flex justify-center items-center flex-col gap-y-2">
-                    <h1 className="text-8xl font-bold">Brain Games 2024</h1>
-                    <h4 className="text-2xl font-semibold max-w-[75%] text-center">
+                    <h1 className="text-6xl md:text-8xl font-bold text-center">Brain Games 2024</h1>
+                    <h4 className="text-xl md:text-2xl font-semibold max-w-[75%] text-center">
                         ICS Student {"Council's"} fun and social competition
                         consisting of a variety of interesting puzzles and games
                         — free lunch, prizes, and exciting events included!
                     </h4>
                 </div>
                 <div className="flex-center gap-x-8">
-                    <div className="flex-center flex-row gap-x-2 text-2xl font-bold">
+                    <div className="flex-center flex-row gap-x-2 text-xl md:text-2xl font-bold">
                         <Calendar />
                         <p>February 10</p>
                     </div>
-                    <div className="flex-center flex-row gap-x-2 text-2xl font-bold">
+                    <div className="flex-center flex-row gap-x-2 text-xl md:text-2xl font-bold">
                         <Navigation />
                         <p>Student Center</p>
                     </div>
                 </div>
             </div>
-
+        
             <div>
                 <Link
                     href={
